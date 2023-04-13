7095459115a6adf8d0f7a371b0c2a26f33ca747d
Philip Austin <121540204+TheHogarthWorkshop@users.noreply.github.com>
2023-04-13 22:18:29 +0100

    v1.0 Beta - Working

--- app.py
+++ app.py
@@ -15,41 +15,61 @@ def index():
         product_description = request.form["product_description"]
         writing_style = request.form["writing_style"]
         additional_info = request.form.getlist("additional_info")
+        marketplace_style = request.form["marketplace_style"]
+
 
         # Store user input history in the session
         if "history" not in session:
             session["history"] = []
 
-        session["history"].append({"product_description": product_description, "writing_style": writing_style, "additional_info": additional_info})
+        session["history"].append({
+            "product_description": product_description, 
+            "writing_style": writing_style, 
+            "additional_info": additional_info, 
+            "marketplace_style": marketplace_style
+        })
         session.modified = True
 
         messages = [
-            {"role": "system", "content": "You are a helpful assistant that generates Etsy product titles, descriptions, and tags."},
-            {"role": "user", "content": f"Generate an Etsy product title, description with writing style '{writing_style}', and tags (separated by commas) for the following product: {product_description}. Please provide the result in the format: TITLE: ... | DESCRIPTION: ... | TAGS: ..."}
+            {"role": "system", "content": "You are a helpful assistant that generates product titles, descriptions, bullet points and tags in the following format: TITLE: ... | DESCRIPTION: ... | BULLETS: ... | TAGS: ..."},
+            {"role": "user", "content": f"Please be '{writing_style}' and generate the product title, description and five bullet points (prefixed by emoji and separated by '\\n') and tags (separated by commas) for the following product: {product_description}."}
         ]
 
         for info in additional_info:
-            messages.append({"role": "user", "content": f"The product also has the following feature: {info}"})
+            messages.append({"role": "user", "content": f"Additional Info: The product also has the following feature(s): {info}"})
+
+        # Add the marketplace_style to the messages
+        messages.append({"role": "user", "content": f"Additional Info: The product is intended for the {marketplace_style}."})
+
+        # Conditionally include HTML markup instruction for Amazon Marketplace
+        if marketplace_style == "Amazon Marketplace":
+            messages.append({"role": "user", "content": "Please include HTML markup ONLY in the description for proper formatting on Amazon Marketplace."})
+
 
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages,
-            temperature=0.6,
+            temperature=0.5,
         )
         result_text = response.choices[0].message["content"]
-        title, description, tags = [x.strip() for x in result_text.split("|")]
+        title, description, bullets, tags = [x.strip() for x in result_text.split("|")]
+
 
         # Remove prefixes
         title = title.replace("TITLE:", "").strip()
         description = description.replace("DESCRIPTION:", "").strip()
+        bullets = bullets.replace("BULLETS:", "").replace("\n", "<br>").strip()
         tags = tags.replace("TAGS:", "").strip()
 
-        return redirect(url_for("index", title=title, description=description, tags=tags))
+        return redirect(url_for("index", title=title, description=description, tags=tags, bullets=bullets))
+
 
     title = request.args.get("title")
     description = request.args.get("description")
+    bullets = request.args.get("bullets", "")
     tags = request.args.get("tags")
-    return render_template("index.html", title=title, description=description, tags=tags, history=session.get("history", []))
+    return render_template("index.html", title=title, description=description, tags=tags, bullets=bullets, history=session.get("history", []))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
